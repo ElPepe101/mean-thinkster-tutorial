bebd9e299a5e8984aa8bd54efd6644555a3a344a
ElPepe <aunsoyjoven@hotmail.com>
2016-03-15 22:12:20 -0600

    First setup with express-generator
    Created view: index.ejs & public/javascripts/app.js

--- README.md
+++ README.md
@@ -5,3 +5,12 @@
 
 The goal of this tutorial is to guide you through the creation of a Reddit/Hacker News clone using the MEAN stack. By completing this tutorial, you will gain a basic understanding of the MEAN stack including building a REST interface with **Express.js** on top of **Node.js** and using that interface to perform CRUD operations on a database via an AngularJS frontend.
 
+### Project Specifications
+
+Before beginning work on any project, it's usually a good idea to know what you're building. Below is a basic list of things we want our users to be able to do:
+
+* Create new posts
+* View all posts ordered by upvotes
+* Add comments about a given post
+* View comments for a given post
+* Upvote posts and comments
---
+++ flapper-news/public/javascripts/app.js
@@ -0,0 +1,85 @@
+'use strict'
+var angular = angular ? angular : null
+
+// ······································
+// ·············· MODULE ················
+var app = angular.module('flapperNews', ['ui.router'])
+var config = function ($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'HomeCtrl',
+      controllerAs: 'home'
+    })
+    .state('posts', {
+      url: '/posts/{id}',
+      templateUrl: '/posts.html',
+      controller: 'PostsCtrl',
+      controllerAs: 'posts'
+    })
+
+  $urlRouterProvider.otherwise('home')
+}
+app.config(['$stateProvider', '$urlRouterProvider', config])
+
+// ······································
+// ············· SERVICE ················
+var posts = function () {
+  var posts = {
+    posts: []
+  }
+
+  return posts
+}
+app.factory('posts', [posts])
+
+// ·············································
+// ············ HOME CONTROLLER ················
+var HomeCtrl = function (posts) {
+  var self = this
+
+  self.posts = posts.posts
+
+  self.addPost = function () {
+    if (!self.title || self.title === '') return
+
+    self.posts.push(
+      {
+        title: self.title,
+        link: self.link,
+        upvotes: 0,
+        comments: [
+          {author: 'Joe', body: 'Cool post!', upvotes: 0},
+          {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+        ]
+      }
+    )
+    self.title = ''
+    self.link = ''
+  }
+
+  self.incrementUpvotes = function (post) {
+    post.upvotes += 1
+  }
+}
+app.controller('HomeCtrl', ['posts', HomeCtrl])
+
+// ··············································
+// ············ POSTS CONTROLLER ················
+var PostsCtrl = function ($stateParams, posts) {
+  var self = this
+
+  self.post = posts.posts[$stateParams.id]
+
+  self.addComment = function () {
+    if (self.body === '') return
+    self.post.comments.push({
+      body: self.body,
+      author: 'user',
+      upvotes: 0
+    })
+    self.body = ''
+  }
+}
+app.controller('PostsCtrl', ['$stateParams', 'posts', PostsCtrl])
