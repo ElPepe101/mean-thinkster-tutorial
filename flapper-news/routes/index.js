3480dd0f56de0930b92184105bd48957c8d0439f
ElPepe <aunsoyjoven@hotmail.com>
2016-03-16 16:10:18 -0600

    Added functionality for up vote posts

--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -5,19 +5,51 @@ var mongoose = require('mongoose')
 var Post = mongoose.model('Post')
 var Comment = mongoose.model('Comment')
 
-router.get('/posts', function (req, res, next) {
+var paramPost = function (req, res, next, id) {
+  var query = Post.findById(id)
+  query.exec(function (err, post) {
+    if (err) return next(err)
+    if (!post) return next(new Error('can\'t find post'))
+    req.post = post
+    return next()
+  })
+}
+router.param('post', paramPost)
+
+// curl http://localhost:3000/posts
+var getPosts = function (req, res, next) {
   Post.find(function (err, posts) {
     if (err) return next(err)
     res.json(posts)
   })
-})
+}
+router.get('/posts', getPosts)
 
-router.post('/posts', function (req, res, next) {
+// curl http://localhost:3000/posts/<POST ID>
+var getPostsPost = function (req, res) {
+  res.json(req.post)
+}
+router.get('/posts/:post', getPostsPost)
+
+// curl --data 'title=test&link=http://test.com' http://localhost:3000/posts
+var postPosts = function (req, res, next) {
   var post = new Post(req.body)
-  post.save(function (err, post) {
+  var postSave = function (err, post) {
     if (err) return next(err)
     res.json(post)
-  })
-})
+  }
+  post.save(postSave)
+}
+router.post('/posts', postPosts)
+
+// curl -X PUT http://localhost:3000/posts/<POST ID>/upvote
+var putPostsPostUpvote = function (req, res, next) {
+  var reqPostUpvote = function (err, post) {
+    if (err) return next(err)
+    res.json(post)
+  }
+  req.post.upvote(reqPostUpvote)
+}
+router.put('/posts/:post/upvote', putPostsPostUpvote)
 
 module.exports = router
